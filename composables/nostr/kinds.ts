988c4909baf274598850ce287d754b0fba761411
sebdev <sebdeveloper6952@gmail.com>
2024-01-26 11:32:34 +0000

    update kinds

--- composables/nostr/kinds.ts
+++ composables/nostr/kinds.ts
@@ -1,6 +1,7 @@
 import { NDKKind } from "@nostr-dev-kit/ndk";
 
 export const FeatureRequestKind = 37300 as NDKKind;
-export const SolutionKind = 73001 as NDKKind;
+export const SolutionKind = 37301 as NDKKind;
 export const PledgeKind = 73002 as NDKKind;
 export const CommentKind = 73003 as NDKKind;
+export const PayoutKind = 73004 as NDKKind;
